e09b3f551cf17ce2efb982c9cd2e22c9f88768d5
perman gurbangylyjov <gurbangylyjow@users.noreply.github.com>
2025-04-07 02:02:12 +0300

    Agzabol düwmesiniň goşulmasy

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDvT46sy5HXSpzvhUtkVpdmRW06EXvFwG8",
+  authDomain: "hasap2pay.firebaseapp.com",
+  projectId: "hasap2pay",
+  storageBucket: "hasap2pay.firebasestorage.app",
+  messagingSenderId: "265308969662",
+  appId: "1:265308969662:web:165f718ed7ec14dad49cef"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
